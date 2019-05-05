d00eed62d96b42064f761a7927fbd4a6ff3124cd
AvBorikDeveloper <avboris28@gmail.com>
2019-05-05 17:31:23 +1200

    firebase connection setup

---
+++ src/firebase.js
@@ -0,0 +1,15 @@
+import * as firebase from 'firebase';
+
+const config = {
+    apiKey: "AIzaSyC8OxH_Hi9-1W8gTgFpwW0E3mUxfGZQc-4",
+    authDomain: "forms-test-6e183.firebaseapp.com",
+    databaseURL: "https://forms-test-6e183.firebaseio.com",
+    projectId: "forms-test-6e183",
+    storageBucket: "forms-test-6e183.appspot.com",
+    messagingSenderId: "614368331343",
+    appId: "1:614368331343:web:ccbc5e5d751abb32"
+  };
+
+  firebase.initializeApp(config);
+
+  firebase.database().ref().set('Bearka, it works, finally');
