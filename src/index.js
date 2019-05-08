95f754da4d3f9e3f944f891476c2251c7bebb661
AvBorikDeveloper <avboris28@gmail.com>
2019-05-08 23:48:24 +1200

    preventing routes

--- src/index.js
+++ src/index.js
@@ -4,22 +4,16 @@ import Routes from './routes';
 import { BrowserRouter } from 'react-router-dom';
 import { firebase } from './firebase';
 
-const App = () =>{
+const App = (props) =>{
+    //console.log(props)
     return(
         <BrowserRouter>
-            <Routes/>
+            <Routes {...props}/>
         </BrowserRouter>
     )
 }
 
-ReactDOM.render(<App />, document.getElementById('root'));
-
 firebase.auth().onAuthStateChanged((user)=>{
-    if(user){
-        console.log('logged in')
-    }else {
-        console.log('logged out')
-    }
-
+   ReactDOM.render(<App auth={user} />, document.getElementById('root'));
 })
 
--- src/routes.js
+++ src/routes.js
@@ -1,13 +1,32 @@
 import React from 'react';
-import { Route, Switch } from 'react-router-dom';
+import { Route, Switch, Redirect } from 'react-router-dom';
 
 import Header from './components/Header/header';
 import Uncontrolled from './components/uncontrolled';
 import Controlled from './components/controlled';
 import User from './components/user';
-import Login from './components/login'
+import Login from './components/login';
+import Dashboard from './components/dashboard';
 
-const Routes = () => {
+const PrivateRoute = ({
+    isLogged,
+    component: Comp,
+    ...rest
+}) => {
+    // console.log(props)
+    // return (
+    //     <div></div>
+    // )
+    return <Route {...rest} component={(props)=>(
+        isLogged ?
+            <Comp {...props}/>
+        : 
+            <Redirect to = "/login"/>
+    )}/>
+}
+
+const Routes = (props) => {
+    
     return (
         <div>
             <Header/>
@@ -16,6 +35,7 @@ const Routes = () => {
                 <Route path="/controlled" exact component={Controlled} />
                 <Route path="/user" exact component={User} />
                 <Route path="/login" exact component={Login}/>
+                <PrivateRoute isLogged={props.auth} path="/dashboard" exact component={Dashboard}/>
             </Switch>
         </div>
     )
